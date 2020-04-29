c27a5aaba46132935b101db311413faafa97b59d
ilkeroralkasim <ilker.oralkasim@gmail.com>
2020-04-29 20:02:32 +0200

    File name changed.

--- iodeepnet/naiveneuralnetwork.py
+++ iodeepnet/naiveneuralnetwork.py
@@ -6,3 +6,17 @@ class DeepNN:
 
   def train(self, x_train, y_train):
     return 0
+
+
+def tanh(Z):
+  return np.tanh(Z)
+
+
+def relu(Z):
+  return np.max(0, Z)
+
+
+def sigmoid(Z):
+  return 1 / (1 + np.exp(-Z))
+
+
