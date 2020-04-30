441fee41f122969a9d88a606e973ad2e143e1470
ilkeroralkasim <ilker.oralkasim@gmail.com>
2020-04-30 22:55:10 +0200

    Basic RNN Class updated.

--- deeplearning/deeplearning.py
+++ deeplearning/deeplearning.py
@@ -7,7 +7,11 @@ def sigmoid(X):
 
 
 def tanh(X):
-    return tanh(X)
+    """
+
+    :rtype: object
+    """
+    return np.tanh(X)
 
 
 def softmax(X):
@@ -18,26 +22,85 @@ def softmax(X):
 # Implementation of Neural Network Architectures:
 
 
-class BasicRNN():
+class BasicRNN:
     def __init__(self, layer_size):
         self.layer_size = layer_size
+        self.parameters  = {}
+
+#   Cell class for basic RNN network. Class will be generalized to include GRU and LSTM, will be a standalone Class.
+    class Cell:
+        def __init__(self, na, nx, ny):
+            self.params = {}
+            self.na = na
+            self.nx = nx
+            self.ny = ny
+
+        def __initialize_parameters(self):
+            self.na = a_prev.shape[0]
+            self.params['Waa']  = np.random.randn(self.na, self.na)
+            self.params['Wax']  = np.random.randn(self.na, self.nx)
+            self.params['Wya']  = np.random.randn(self.ny, self.nx)
+            self.params['ba']   = np.random.randn(self.na, 1)
+            self.params['by']   = np.random.randn(self.ny, 1)
+
+        def get_parameters():
+            return self.params
+
+
+        def update(self, a_prev):
+
+
+        def run_cell(self):
+            return
+
+    def train(self, X, Y, epochs=1000):
+        nx, m, Tx = X.shape
+        ny, m, Ty = Y.shape
+        a0 = np.random.randn(self.na,m)
+
+
+        # Create the network, with Tx RNN cells.
+        for i in range(Tx):
+            cells.append(Cell(self.layer_size, nx, ny))
+
+
+        cells = []
+        a[:, :, 0] = cells[0].update(a0, X)
+
+        a[:, :, 0] = np.random.randn(self.na, m)
+
+        for epoch in epochs:
+            for t in range(Tx):
+                a[:, :, t+1] = cells[t].update(a[:, :, t], X[:, :, t])
+
+
+    def __forward_propagate(self):
+        # Forward propagation through T<X>
+
+        for t in range(Tx-1):
+            self.a[:, :, t+1] = tanh(np.dot(self.parameters['Waa'], self.a[:, :, t]) +
+                                     np.dot(self.parameters['Wax'], x_train[:, :, t+1]) +
+                                     np.dot(self.parameters['ba'])
+
+            y_predict[:, :, t+1] = softmax(np.dot(self.parameters['Wya'], a[:, :, t+1]) +
+                                           self.parameters['by'])
+
+
+            self.caches.append(cache)
+            return y_predict
 
     def train(self, x_train, y_train, seed=None, epochs=10, callbacks=[]):
-        W = {}
+
+        # Get the required dimensions for training
         nx, m, Tx = x_train.shape
-        a = np.zeros((self.layer_size, m, Tx))
+        ny, Ty = y_train.shape[0], y_train.shape[0]
+
+        self.__initialize_parameters(na, nx, ny, m, Tx)
+
+        y_predict = np.zeros((ny, m, Ty))
+
         na = self.layer_size
 
-        # Initialize Parameters Waa, Wax, Wya
-
-        if seed:
-            np.random.seed(seed)
-        W['aa'] = np.random.randn(na, na)
-        W['ax'] = np.random.randn(na, nx)
-        W['ya'] = np.random.randn(nx, na)
-
-        for k in W.keys():
-            print(W[k][4][1])
 
 
 class NaiveNeuralNetwork():
--- deeplearning/test.py
+++ deeplearning/test.py
@@ -2,5 +2,3 @@ import deeplearning
 import numpy as np
 
 model = deeplearning.BasicRNN(200)
-np.random.seed(1)
-model.train(np.random.randn(3, 10, 4), np.random.randn(3, 10, 4), seed=1)
